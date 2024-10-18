082a7f5fbd2b9ab51683bb5e25fb10f1a5ae47c5
Lavanya Mittal <lavamitt@berkeley.edu>
2024-10-18 10:12:12 -0400

    pass stage4

--- src/codeemission.rs
+++ src/codeemission.rs
@@ -1,6 +1,5 @@
 use crate::codegen::{
-    AssemblyBinaryOperator, AssemblyFunctionDefinition, AssemblyInstruction, AssemblyProgram,
-    AssemblyUnaryOperator, Operand, Reg,
+    AssemblyBinaryOperator, AssemblyFunctionDefinition, AssemblyInstruction, AssemblyProgram, AssemblyUnaryOperator, ConditionalCode, Operand, Reg
 };
 const INDENT: &str = "    ";
 
@@ -27,30 +26,55 @@ fn emit_instructions(instructions: &[AssemblyInstruction], assembly: &mut String
             AssemblyInstruction::Mov(src, dst) => {
                 assembly.push_str(INDENT);
                 assembly.push_str("movl ");
-                emit_operand(src, assembly);
+                emit_operand(src, assembly, OperandNumBytes::Four);
                 assembly.push_str(", ");
-                emit_operand(dst, assembly);
+                emit_operand(dst, assembly, OperandNumBytes::Four);
                 assembly.push_str("\n");
             }
             AssemblyInstruction::Unary(unop, operand) => {
                 assembly.push_str(&format!("{}{} ", INDENT, unop_to_assembly_str(unop)));
-                emit_operand(operand, assembly);
+                emit_operand(operand, assembly, OperandNumBytes::Four);
                 assembly.push_str("\n");
             }
             AssemblyInstruction::Binary(binop, src, dst) => {
                 assembly.push_str(&format!("{}{} ", INDENT, binop_to_assembly_str(binop)));
-                emit_operand(src, assembly);
+                emit_operand(src, assembly, OperandNumBytes::Four);
                 assembly.push_str(", ");
-                emit_operand(dst, assembly);
+                emit_operand(dst, assembly, OperandNumBytes::Four);
                 assembly.push_str("\n");
             }
             AssemblyInstruction::Idiv(operand) => {
                 assembly.push_str("idivl ");
-                emit_operand(operand, assembly);
+                emit_operand(operand, assembly, OperandNumBytes::Four);
                 assembly.push_str("\n");
             }
             AssemblyInstruction::Cdq => {
                 assembly.push_str("cdq\n");
+            }
+            AssemblyInstruction::Cmp(op1, op2) => {
+                assembly.push_str(&format!("{}cmpl ", INDENT));
+                emit_operand(op1, assembly, OperandNumBytes::Four);
+                assembly.push_str(", ");
+                emit_operand(op2, assembly, OperandNumBytes::Four);
+                assembly.push_str("\n");
+            }
+            AssemblyInstruction::Jmp(label) => {
+                let local_label = &format!(".L{}", label);
+                assembly.push_str(&format!("{}jmp {}\n", INDENT, local_label));
+            }
+            AssemblyInstruction::JmpCC(cond_code, label) => {
+                let local_label = &format!(".L{}", label);
+                let code = cond_code_to_assembly_str(cond_code);
+                assembly.push_str(&format!("{}j{} {}\n", INDENT, code, local_label));
+            }
+            AssemblyInstruction::SetCC(cond_code, operand) => {
+                let code = cond_code_to_assembly_str(cond_code);
+                assembly.push_str(&format!("{}set{} ", INDENT, code));
+                emit_operand(operand, assembly, OperandNumBytes::One);
+                assembly.push_str("\n");
+            }
+            AssemblyInstruction::Label(label) => {
+                assembly.push_str(&format!(".L{}:\n", label));
             }
             AssemblyInstruction::AllocateStack(offset) => {
                 assembly.push_str(&format!("{}subq ${}, %rsp\n", INDENT, offset));
@@ -65,12 +89,17 @@ fn emit_instructions(instructions: &[AssemblyInstruction], assembly: &mut String
     }
 }
 
-fn emit_operand(operand: &Operand, assembly: &mut String) {
+pub enum OperandNumBytes {
+    One,
+    Four
+}
+
+fn emit_operand(operand: &Operand, assembly: &mut String, num_bytes: OperandNumBytes) {
     let constant = match operand {
         Operand::Imm(num) => {
             format!("${}", num)
         }
-        Operand::Register(reg) => reg_to_assembly_str(reg).to_string(),
+        Operand::Register(reg) => reg_to_assembly_str(reg, num_bytes).to_string(),
         Operand::Stack(offset) => format!("{}(%rbp)", offset),
         _ => panic!("Found unknown operand"),
     };
@@ -94,11 +123,42 @@ fn binop_to_assembly_str(binop: &AssemblyBinaryOperator) -> &str {
     }
 }
 
-fn reg_to_assembly_str(reg: &Reg) -> &str {
+fn reg_to_assembly_str(reg: &Reg, num_bytes: OperandNumBytes) -> &str {
     match reg {
-        Reg::AX => "%eax",
-        Reg::DX => "%edx",
-        Reg::R10 => "%r10d",
-        Reg::R11 => "%r11d",
+        Reg::AX => {
+            match num_bytes {
+                OperandNumBytes::Four => "%eax",
+                OperandNumBytes::One => "%al",
+            }
+        }
+        Reg::DX => {
+            match num_bytes {
+                OperandNumBytes::Four => "%edx",
+                OperandNumBytes::One => "%dl",
+            }
+        }
+        Reg::R10 => {
+            match num_bytes {
+                OperandNumBytes::Four => "%r10d",
+                OperandNumBytes::One => "%r10b",
+            }
+        }
+        Reg::R11 => {
+            match num_bytes {
+                OperandNumBytes::Four => "%r11d",
+                OperandNumBytes::One => "%r11b",
+            }
+        }
+    }
+}
+
+fn cond_code_to_assembly_str(code: &ConditionalCode) -> &str {
+    match code {
+        ConditionalCode::E => "e",
+        ConditionalCode::NE => "ne",
+        ConditionalCode::G => "g",
+        ConditionalCode::GE => "ge",
+        ConditionalCode::L => "l",
+        ConditionalCode::LE => "le",
     }
 }
